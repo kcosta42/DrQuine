162bd5541dda3c387b68537c3ecdcb861ee9cbd0
Kevin COSTA <kcosta@e3r4p17.42.fr>
2019-04-10 14:52:08 +0200

    Remove argv in Sully

--- sources/Colleen.c
+++ sources/Colleen.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
-// Useless Function
+/*
+	Useless Function
+*/
 void func(void) {}
 
 int main(void)
 {
-	// Main Function
-	char *c = "#include <stdio.h>%1$c%1$c// Useless Function%1$cvoid func(void) {}%1$c%1$cint main(void)%1$c{%1$c%2$c// Main Function%1$c%2$cchar *c = %3$c%s%3$c;%1$c%2$cfunc();%1$c%2$cprintf(c, 10, 9, 34, c);%1$c%2$creturn (0);%1$c}%1$c";
+	/*
+		Main Function
+	*/
+	char *c = "#include <stdio.h>%1$c/*%1$c%2$cUseless Function%1$c*/%1$cvoid func(void) {}%1$c%1$cint main(void)%1$c{%1$c%2$c/*%1$c%2$c%2$cMain Function%1$c%2$c*/%1$c%2$cchar *c = %3$c%s%3$c;%1$c%2$cfunc();%1$c%2$cprintf(c, 10, 9, 34, c);%1$c%2$creturn (0);%1$c}%1$c";
 	func();
 	printf(c, 10, 9, 34, c);
 	return (0);
--- sources/Grace.c
+++ sources/Grace.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-#define GRACE int main(void) { FILE *fs=fopen(NAME, "w"); fprintf(fs, CHAR, 10, 34, CHAR); fclose(fs); return (0); }
+#define GRACE int main(void) { FILE *fs=fopen(NAME, "w"); fprintf(fs, CHAR, 10, 34, CHAR, 9); fclose(fs); return (0); }
 #define NAME "Grace_kid.c"
-#define CHAR "#include <stdio.h>%1$c%1$c#define GRACE int main(void) { FILE *fs=fopen(NAME, %2$cw%2$c); fprintf(fs, CHAR, 10, 34, CHAR); fclose(fs); return (0); }%1$c#define NAME %2$cGrace_kid.c%2$c%1$c#define CHAR %2$c%3$s%2$c%1$c%1$c// Let's the hack begin%1$cGRACE%1$c"
-
-// Let's the hack begin
+#define CHAR "#include <stdio.h>%1$c%1$c#define GRACE int main(void) { FILE *fs=fopen(NAME, %2$cw%2$c); fprintf(fs, CHAR, 10, 34, CHAR, 9); fclose(fs); return (0); }%1$c#define NAME %2$cGrace_kid.c%2$c%1$c#define CHAR %2$c%3$s%2$c%1$c/*%1$c%4$cLet's the hack begin%1$c*/%1$cGRACE%1$c"
+/*
+	Let's the hack begin
+*/
 GRACE
