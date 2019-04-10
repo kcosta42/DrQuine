7b9ed59a330ab8ec0e59901902bd23fbe510ac1f
Kevin COSTA <kcosta@e3r4p17.42.fr>
2019-04-10 15:01:18 +0200

    Added protection if fopen fail

--- sources/Grace.c
+++ sources/Grace.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-#define GRACE int main(void) { FILE *fs=fopen(NAME, "w"); fprintf(fs, CHAR, 10, 34, CHAR, 9); fclose(fs); return (0); }
+#define FT(x)int main(void) { FILE *fs=fopen(NAME, "w"); if (!fs) return (-1); fprintf(fs, CHAR, 10, 34, CHAR, 9); fclose(fs); return (0); }
 #define NAME "Grace_kid.c"
-#define CHAR "#include <stdio.h>%1$c%1$c#define GRACE int main(void) { FILE *fs=fopen(NAME, %2$cw%2$c); fprintf(fs, CHAR, 10, 34, CHAR, 9); fclose(fs); return (0); }%1$c#define NAME %2$cGrace_kid.c%2$c%1$c#define CHAR %2$c%3$s%2$c%1$c/*%1$c%4$cLet's the hack begin%1$c*/%1$cGRACE%1$c"
+#define CHAR "#include <stdio.h>%1$c%1$c#define FT(x)int main(void) { FILE *fs=fopen(NAME, %2$cw%2$c); if (!fs) return (-1); fprintf(fs, CHAR, 10, 34, CHAR, 9); fclose(fs); return (0); }%1$c#define NAME %2$cGrace_kid.c%2$c%1$c#define CHAR %2$c%3$s%2$c%1$c/*%1$c%4$cLet's the hack begin%1$c*/%1$cFT(xxxxxxxx)%1$c"
 /*
 	Let's the hack begin
 */
-GRACE
+FT(xxxxxxxx)
